8d41fe63e22b5350d7e81859f88beaf57e3bf8b9
Bryan Matthew <bryanmatthew206@gmail.com>
2023-09-06 21:43:15 +0800

    feat: setup router for portfolio page

--- personal-website/src/router.jsx
+++ personal-website/src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./views/Home";
 import About from "./views/About";
+import Portfolio from "./views/Portfolio";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
             {
                 path: "/about",
                 element: <About />,
+            },
+            {
+                path: "/portfolio",
+                element: <Portfolio />,
             },
         ],
     },
